a6596e14171d2d3aabab6bf66317c2aedf0a1715
Nsakrty <119097728+Nsakrty@users.noreply.github.com>
2025-06-10 10:40:51 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
-const twikoo = require('twikoo-vercel')
+const twikoo = require('twikoo-vercel');
 
 export default async function handler(req, res) {
   // 添加 CORS 支持
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-  if (req.method === 'OPTIONS') return res.status(204).end()
+  res.setHeader('Access-Control-Allow-Origin', 'https://srpn.top'); // 或者指定你的域名，如 ''
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(204).end();
 
   // 调用 Twikoo 主体逻辑
-  return twikoo(req, res)
+  return twikoo(req, res);
 }
