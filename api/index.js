836e995d0f660637115ca56e9fd1ee12f5478b61
Nsakrty <119097728+Nsakrty@users.noreply.github.com>
2025-06-14 09:26:38 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,9 @@
 const twikoo = require('twikoo-vercel');
 
 export default async function handler(req, res) {
-  // 允许跨域请求
   res.setHeader('Access-Control-Allow-Origin', 'https://srpn.top');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  // 如果是预检请求，直接返回 204（必须处理，不然浏览器请求会失败）
-  if (req.method === 'OPTIONS') {
-    return res.status(204).end();
-  }
-
-  // Twikoo 主体逻辑
+  if (req.method === 'OPTIONS') return res.status(204).end();
   return twikoo(req, res);
 }
